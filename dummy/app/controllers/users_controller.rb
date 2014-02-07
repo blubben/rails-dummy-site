8445fe648332f69012da78d3c95de1e359cc6dc9
Brian Lubben <blubben@gmail.com>
2014-02-07 02:17:16 -0500

    Added sign in feature

--- dummy/app/controllers/users_controller.rb
+++ dummy/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if(signed_in?)
+      @user = User.find(params[:id])
+    else
+      redirect_to '/signin'
+    end
   end
   def new
     @user = User.new
@@ -9,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
